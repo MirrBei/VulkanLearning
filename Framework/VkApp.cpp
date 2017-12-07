1c7927798dd439a8da159af42637202543faeb6b
fuhao <1119369173@qq.com>
2017-12-08 00:31:58 +0800

    建立项目，完成物理设备创建、物理设备内存和队列信息查询

---
+++ Framework/VkApp.cpp
@@ -0,0 +1,41 @@
+﻿#include "VkApp.h"
+
+VkResult VkApp::init()
+{
+    VkApplicationInfo appInfo = {};
+    VkInstanceCreateInfo instanceCreateInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.applicationVersion = 1;
+    appInfo.pApplicationName = "VulkanLearning";
+
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pApplicationInfo = &appInfo;
+
+    VkResult result = vkCreateInstance(&instanceCreateInfo, nullptr, &instance_);
+    if (result == VK_SUCCESS)
+    {
+        uint32_t deviceCount = 0;
+        result = vkEnumeratePhysicalDevices(instance_, &deviceCount, nullptr);
+        if (result == VK_SUCCESS)
+        {
+            physicalDevices_.resize(deviceCount);
+            result = vkEnumeratePhysicalDevices(instance_, &deviceCount, physicalDevices_.data());
+        }
+    }
+    return result;
+}
+
+void VkApp::queryPhysicalDeviceProperties(const int physicalDeviceIndex)
+{
+    if (physicalDeviceIndex < 0 || physicalDeviceIndex >= physicalDevices_.size())
+        return;
+    const VkPhysicalDevice physicalDevice = physicalDevices_[physicalDeviceIndex];
+    VkPhysicalDeviceMemoryProperties memoryProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memoryProperties);
+
+    uint32_t queueFamilyCount;
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilyProperties.data());
+}
---
+++ Framework/VkApp.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "vulkan/vulkan.hpp"
+
+class VkApp
+{
+public:
+    VkResult init();
+    void queryPhysicalDeviceProperties(int physicalDeviceIndex);
+
+private:
+    VkInstance instance_ = nullptr;
+    std::vector<VkPhysicalDevice> physicalDevices_;
+};
